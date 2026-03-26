70054c86261e5544d89f3e4db37664c163bbab9f
Ori Mwaniki-Alexander <hmwaniki@pratt.edu>
2026-03-26 02:13:56 -0400

    Update MinorityDegreeREV_OMA.R
    updating path, formatting

--- MinorityDegreeREV_OMA.R
+++ MinorityDegreeREV_OMA.R
@@ -6,60 +6,58 @@ library(corrplot)
 library(ggplot2)
 library(ggcorrplot)
 
-# Load data
-setwd("C:/Users/hanna/Onedrive/Desktop/projfile") 
+# load data
+setwd("C:/Users/...") 
 revenue <- read_csv("tuition.csv")
 
-# Filter missing data
+# filter missing data
 revenue <- na.omit(revenue)
 ls(revenue)
 revenue <- revenue %>% select(-c("Institution_Name","UnitID"))
 
-#new variables
+# new variables
 blkdegree <- revenue$Black_Degree/revenue$totdegree *100
 hisdegree <- revenue$hispanicdegrees/revenue$totdegree *100
 year <- revenue$Year
 totrev <- revenue$totrev
 
-#create smaller data
+# create smaller data
 blkhisrev <- revenue %>% select(-c("instate_tuition", "Year","totdegree"))
 ls(blkhisrev)
 View(blkhisrev)
 
-#scatterplot
+# scatterplot
 pairs(blkhisrev, main = "Black and Hispanic Enrollment and Public Institution Revenues", upper.panel = NULL)
 ls(blkhisrev)
 
-#correlation
-# Example data: replace 'your_data' with your actual dataset
-# Compute correlation matrix
+# correlation v1
+mtrx <- cor(blkhisrev)
+corrplot(mtrx)
+
+# correlation matrix v2
 cor_matrixmod2 <- cor(blkhisrev)
 
-# Rename the row and column names in the correlation matrix
+# rename the row and column names in the correlation matrix
 colnames(cor_matrixmod2) <- c("Revenue", "Black", "Hispanic", "Total Students", "Student Aid", "Federal Funding")  # New names
 rownames(cor_matrixmod2) <- c("Revenue", "Black", "Hispanic", "Total Students", "Student Aid", "Federal Funding")
 
 corrplot(cor_matrixmod2,
-         method = "circle",     # Type of visualization ('ellipse', 'number', 'shade', etc.)e
-         tl.col = "black",      # Text label color
-         tl.srt = 45,           # Text label rotation
-         col = colorRampPalette(c("orange3", "pink", "#5548e4"))(200) # Gradient color scale
-       )   # Adjust margins
+         method = "circle",     # type of visualization
+         tl.col = "black",      
+         tl.srt = 45,           
+         col = colorRampPalette(c("orange3", "pink", "#5548e4"))(200) # gradient color scale
+       )   
 
-#correlation
-mtrx <- cor(blkhisrev)
-corrplot(mtrx)
-
-#multivariate regression
+# multivariate regression
 multi_revs <- lm(blkhisrev$totrev ~ blkhisrev$Black_Degree + blkhisrev$hispanicdegrees, data=blkhisrev)
 summary(multi_revs)
 
-#model 1
+# model 1
 ls(revenue)
 revenue_mod1 <- revenue %>% select(-c("FederalFunding", "FederalFunding", "StudentFedAid_Percent","Year","instate_tuition", "Total_Students", "totdegree"))
 ls(revenue_mod1)
 
-#scatterplot
+# scatterplot
 pairs(revenue_mod1, main = "Black and Hispanic Degree Attainment and Public Institution Revenues", upper.panel = NULL)
 
 pairs(revenue_mod1,
@@ -69,50 +67,48 @@ pairs(revenue_mod1,
       gap = 0.5,
       upper.panel = NULL)  # Reduce gap between panels
 
-#correlation
-# Example data: replace 'your_data' with your actual dataset
-# Compute correlation matrix
+# correlation
 cor_matrix <- cor(revenue_mod1)
 
-# Rename the row and column names in the correlation matrix
+# rename the row and column names in the correlation matrix
 colnames(cor_matrix) <- c("Total Revenue", "Black Degree", "Hispanic Degree")  # New names
 rownames(cor_matrix) <- c("Total Revenue", "Black Degree", "Hispanic Degree")
 
 
 corrplot(cor_matrix, 
          main = "Model 1: Black and Hispanic Enrollment and Public Institution Revenues",
-         method = "circle",     # Type of visualization ('ellipse', 'number', 'shade', etc.)
-         type = "lower",        # Show only lower triangle
-         tl.col = "black",      # Text label color
-         tl.srt = 45,           # Text label rotation
-         addCoef.col = "black", # Add correlation coefficients
-         col = colorRampPalette(c("magenta", "#edeced", "#5548e4"))(200), # Gradient color scale
+         method = "circle",     
+         type = "lower",        
+         tl.col = "black",     
+         tl.srt = 45,          
+         addCoef.col = "black", 
+         col = colorRampPalette(c("magenta", "#edeced", "#5548e4"))(200), 
          title = "Correlation Matrix",
-         mar = c(0, 0, 1, 0))   # Adjust margins
+         mar = c(0, 0, 1, 0))   
 
 
-#HBCU vs PWI
-setwd("C:/Users/hanna/Onedrive/Desktop/projfile") 
+# HBCU vs PWI
+setwd("C:/Users/...") 
 hbcu <- read.csv("HBCU.csv")
 View(hbcu)
 hbcu <-na.omit(hbcu)
 
 hbcurev <- hbcu$HBCUrevstudent
 
-#descriptive statistics hbcu revenue per student
+# descriptive statistics hbcu revenue per student
 mean(hbcurev)
 median(hbcurev)
 sd(hbcurev)
 var(hbcurev)
 
-setwd("C:/Users/hanna/Onedrive/Desktop/projfile") 
+setwd("C:/Users/...") 
 pwi <- read.csv("AVGstudentrev.csv")
 pwi <- na.omit(pwi)
 View(pwi)
 
 pwirev <- pwi$RevenueperStudent
 
-#descriptive statistics pwi revenue per student
+# descriptive statistics pwi revenue per student
 mean(pwirev)
 median(pwirev)
 sd(pwirev)
@@ -127,7 +123,7 @@ ggplot(hbcu, aes(x = hbcurev)) +
   geom_histogram(fill = "sienna3", color = "white") +
   labs(title = "HBCU Revenue per Student", x = "Revenue per Student", y = "Frequency")
 
-# Create the histogram
+# create the histogram
 ggplot(pwi, aes(x = pwirev)) +
   geom_histogram(binwidth = 8000, fill = "#5548e4", color = "white", alpha = 0.9) +
   labs(title = "HBCU Revenue per Student", x = "Revenue per Student", y = "Frequency") +
